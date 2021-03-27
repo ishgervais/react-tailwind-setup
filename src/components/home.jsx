046e173a44719e07c0f93793468f6c9b47523a96
Gervais Ishimwe <54812621+ishgervais@users.noreply.github.com>
2021-03-27 13:35:57 +0200

    Add files via upload
    Initial commit with tailwind

---
+++ src/components/home.jsx
@@ -0,0 +1,7 @@
+function HomePage(){
+    reutrn(
+        <div className="bg-dark">
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Obcaecati illum minima praesentium asperiores corporis cupiditate expedita accusamus libero sed, eligendi odit hic quia dolores dolore, eaque voluptatibus incidunt perspiciatis vel!
+        </div>
+    );
+}
